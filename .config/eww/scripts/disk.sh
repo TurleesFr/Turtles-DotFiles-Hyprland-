e328aa1eadd3b48f05b89a6b426f3142442ad40e
TurleesFr <brickaltacc@gmail.com>
2025-08-26 11:13:21 +0200

    Delete .config/eww/scripts directory

--- .config/eww/scripts/disk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Get used and total space in KB for /home
-read used total <<< $(df /home | awk 'NR==2 {print $3, $2}')
-# Calculate percentage
-percent=$(( 100 * used / total ))
-echo "$percent"
